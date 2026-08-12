c6bcd1bf13603609a588612eb0be989ce480d27a
pagotisaipujitha <pagotisaipujitha1@gmail.com>
2026-08-12 20:17:15 +0530

    Create Parameters.java

---
+++ Practice Programs/Parameters/Parameters.java
@@ -0,0 +1,27 @@
+package myprogs;
+
+public class Parameters {
+
+		
+		void add() {
+			int x=5, y=6;
+			System.out.println("Add is : " + (x+y));
+		}
+		
+		void addition(int c) {
+			int a=5, b=6;
+			System.out.println("Addition of 3 values is: " + (a+b+c));
+		}
+		
+		//method with params and return type(int)
+		int sum(int x, int y, int z) {
+			return x+y+z+100;
+		}
+		
+		public static void main(String[] args) {
+			Parameters pm = new Parameters();
+			int res = pm.sum(12, 34, 56);
+			System.out.println("Result is: " + res);
+		}
+	}
+
